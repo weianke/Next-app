a98fc55d0c5e7db293d425722281dbaff202295f
anke <245783065@qq.com>
2020-06-07 19:44:37 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
+
+  server.use(async (ctx, next) => {
+    await handle(ctx.req, ctx.res);
+    ctx.responed = false;
+  });
+
+  server.listen(3000, () => {
+    console.log('koa server listening 3000');
+  });
+});
